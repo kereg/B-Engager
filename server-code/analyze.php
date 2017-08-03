9aca92b246622c8605323f52101dd8e4d6b3b1a1
Gal Keren <gal@bidalgo.com>
2017-08-03 13:30:35 +0300

    new stuff

--- server-code/analyze.php
+++ server-code/analyze.php
@@ -10,12 +10,14 @@ require_once $_SERVER['DOCUMENT_ROOT']."/B-Engager/server-code/config.php";
 require_once $_SERVER['DOCUMENT_ROOT']."/B-Engager/server-code/page/AnalyzePage.php";
 use page\AnalyzePage;
 
-
+//$link = mysqli_connect("10.0.0.66","root","13131313","prosper");
 
 try{
     $analyze = new AnalyzePage($pageId,$accessToken,$postIds);
     $data = $analyze->analyzePage();
-    echo json_encode($data);
+    $jsonData = json_encode(array("data" => $data));
+    //mysqli_query($link,"INSERT INTO bengager_responses ")
+    echo $jsonData;
 }
 catch (\Exception $e){
     echo $e->getMessage();
